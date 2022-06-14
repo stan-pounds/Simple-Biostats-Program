e665ce0715f904cfbe85cc0c93bd79400331a7e0
Stan Pounds <stanley.pounds@stjude.org>
2022-06-14 10:50:49 -0500

    Set-up script for SBP

---
+++ setup-SBP.R
@@ -0,0 +1,21 @@
+
+SBP.code.path="https://raw.githubusercontent.com/stan-pounds/Simple-Biostats-Program/main/"
+
+# specify the file names
+SBP.code.files=c("get-packages.R",
+               "describe.R","estimate.R",
+               "compare.R","model.R","colors.R",
+               "print.R","figures.R","read-data.R")
+
+SBP.code.files=paste0(SBP.code.path,SBP.code.files)
+
+for (i in 1:length(SBP.code.files))
+  source(SBP.code.files[i])
+
+get.package("survival")
+get.package("cmprsk")
+get.package("penalized")
+get.package("riskRegression")
+get.package("knitr")
+get.package("data.table")
+get.package("readxl")
